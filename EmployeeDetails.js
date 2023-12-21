316abe39eb681da1080f6956b9089127e0b07ad2
udaykumar721 <uday56367@gmail.com.com>
2023-12-21 12:23:06 +0530

    Employee Payroll Form Second Page

---
+++ EmployeeDetails.js
@@ -0,0 +1,24 @@
+class EmployeeDetails{
+    get name(){
+
+    }
+    get profileImage(){
+        
+    }
+    get gender(){
+        
+    }
+    get department(){
+        
+    }
+    get salary(){
+        
+    }
+    get startDate(){
+        
+    }
+    get notes(){
+        
+    }
+}
+export default EmployeeDetails
